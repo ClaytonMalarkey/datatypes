9c3757f3e92db1ef1203a95cad301c63c93430c8
ClaytonMalarkey <malarkey1985@gmail.com>
2023-09-13 19:57:33 -0600

    second commit

---
+++ js/app.js
@@ -0,0 +1,6 @@
+var max_volume = 100;
+var current_volume = 25;
+var result1 = max_volume / current_volume;
+var result2 = (max_volume / current_volume )* 100 ;
+
+console.log("result 1 = " + result1 + " and result 2 = " + result2);
